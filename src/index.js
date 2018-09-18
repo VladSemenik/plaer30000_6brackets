6849503cdce1273c3c75f338391b0196f35f4010
VladSemenik <vlad.semenik@gmail.com>
2018-09-18 22:41:35 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var mas = [];
+  for(var k =0; k<str.length; k++){
+    for(var m = 0; m < bracketsConfig.length; m++){
+         for(var n =0; n< 2; n++){
+             if(bracketsConfig[m][n]==str[k]){
+                         if(n==0 && bracketsConfig[m][1]!=mas[mas.length-1]){
+                              mas.splice(mas.length, 0, bracketsConfig[m][n]);
+                             break;
+                            }
+  
+                              if(bracketsConfig[m][0] == mas[mas.length-1] && n==1){
+                                  if(mas.length!=0)
+                                  mas.splice(mas.length-1, 1);
+                          }else {
+                            if( n== 1){
+                              mas.splice(mas.length, 0, bracketsConfig[m][n]);
+                            }
+                          }
+                     }      
+         }
+      }
+  }
+  if(mas.length==0)return true;
+  else return false;
 }
